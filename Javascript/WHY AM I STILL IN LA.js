f54d75e81235f0567393b7f72fe22e888562eccb
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:48:06 -0400

    Create WHY AM I STILL IN LA.js

---
+++ Javascript/WHY AM I STILL IN LA.js
@@ -0,0 +1,57 @@
+var namelol = "Shadow! ";
+var firstsong = "YEAH RIGHT";
+var secondsong = "WANTED U";
+var thirdsong = "Sanctuary";
+var fourthsong = "TEST DRIVE";
+var fifthsong = "WHY AM I STILL IN LA";
+var sixthsong = "I don't wanna waste my time";
+var seventhsong = "worldstar money (interlude)";
+var eighthsong = "YUKON (INTERLUDE)";
+
+const nextlinetext = "WHY\nAM\nI\nSTILL\nIN\nLA"; //yooo im skidding!!
+console.log(nextlinetext)
+
+const teststring = 'Big\n\tFat\n\t\tNut';
+console.log(teststring);
+
+const nextlinetext3 = "IM\nTIRED\nOF\nLOS\nANGELES";
+console.log(nextlinetext3)
+
+function mcLarenP1() {
+    console.log("My favorite car is a McLaren P1.")
+}
+
+const nextlinetext2 = "WHY\nAM\nI\nSTILL\nIN\nLA";
+console.log(nextlinetext2)
+
+//this page is for practice, so i don't lose the knowledge
+var thestring = "Cory is old lets change the name, " + namelol + "Thats better, lol! ";  //this is not my new name lol, cory is legendary
+
+console.log(thestring);
+
+var songstring = "1st. " + firstsong + "\n 2nd. " + secondsong + "\n 3rd. " + thirdsong + "\n 4th. " + fourthsong + "\n 5th. " + fifthsong + "\n 6th. " + sixthsong + "\n 7th. " + seventhsong + "\n 8th. " + eighthsong + "";
+console.log(songstring)
+
+mcLarenP1();
+
+let histring = "1st. YEAH RIGHT"
+histring += "\n 2nd. WANTED U"
+histring += "\n 3rd. Sanctuary"
+histring += "\n 4th. TEST DRIVE"
+histring += "\n 5th. WHY AM I STILL IN LA"
+histring += "\n 6th. I don't wanna waste my time"
+histring += "\n 7th. worldstar money (interlude)"
+histring += "\n 8th. YUKON (INTERLUDE)"
+
+console.log(histring);
+
+//yeah man idk, thats what im listening too right now
+
+let yeah = 0;
+const yea = "YEAH RIGHT"
+
+yeah = yea.length
+
+console.log(yeah)
+
+mcLarenP1();
