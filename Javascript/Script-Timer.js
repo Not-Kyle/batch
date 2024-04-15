4a1e98d623e3ad4591b8bdf72a4bd400f33663a1
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:46:05 -0400

    Create Script-Timer.js

---
+++ Javascript/Script-Timer.js
@@ -0,0 +1,15 @@
+function tiFun(){
+    for(let b = 0; b < 1000000000; b++){
+    
+    }
+  }
+  console.time("Time");
+  tiFun();
+  console.timeEnd("Time");function tiFun(){
+    for(let b = 0; b < 1000000000; b++){
+    
+    }
+  }
+  console.time("Time");
+  tiFun();
+  console.timeEnd("Time");
