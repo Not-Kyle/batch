475e65a42d713368c449cc9fd78e09d58c7ba581
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:47:50 -0400

    Create Random.js

---
+++ Javascript/Random.js
@@ -0,0 +1,16 @@
+let rando1 = Math.floor(Math.random()*255);
+const strings1 = ".";
+let rando2 = Math.floor(Math.random()*255);
+const strings2 = ".";
+let rando3 = Math.floor(Math.random()*255);
+const strings3 = ".";                               //gotta clean up these lines 
+let rando4 = Math.floor(Math.random()*255);
+
+console.log(rando1,strings1,rando2,strings2,rando3,strings3,rando4);
+
+const ddoslinko = '<a href="http://www.netscout.com/ddos-attack-map" target="_blank">Link</a>';
+
+let randomipgen = Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255)
+
+console.log(randomipgen);
+                                         //heres the clean up
