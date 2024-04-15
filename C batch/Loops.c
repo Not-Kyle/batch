1abc799a9ebf29cd34f5f27c4e0ca7c705cb4a90
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:44:04 -0400

    Update and rename C batch.c to C batch/Loops.c

---
+++ C batch/Loops.c
@@ -0,0 +1,20 @@
+int counter = 0;
+while (counter < 10)
+{
+  Console.WriteLine($"Hello World! The counter is {counter}");
+  counter++;
+}
+
+//////////////////////////////////////
+
+
+for (char column = 'a'; column < 'k'; column++)
+{
+  Console.WriteLine($"The column is {column}");
+}
+
+//----------------Notes------------------------
+
+//The % operator gives you the remainder of a division operation.
+//The if statement gives you the condition to see if a number should be part of the sum.
+//The for loop can help you repeat a series of steps for all the numbers 1 through 20.
