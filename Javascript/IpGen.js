00c4bbc2baa1922aef4d2fa66255be6bf6552dbe
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:45:49 -0400

    Update and rename Javascript batch.js to Javascript/IpGen.js

---
+++ Javascript/IpGen.js
@@ -0,0 +1,5 @@
+let ipGen = Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255) + "." + Math.floor(Math.random()*255)
+
+console.log(ipGen);
+
+//ipGen = 172061505; //the value of total IP's you can generate from this code.
