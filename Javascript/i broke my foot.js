78766075107a032040429c62c28a8b527c679d59
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:48:52 -0400

    Create i broke my foot.js

---
+++ Javascript/i broke my foot.js
@@ -0,0 +1,11 @@
+const brokenFoot = "Footguy";
+
+const smashedfoot = brokenFoot[brokenFoot.length - 1];
+
+console.log(brokenFoot, smashedfoot);
+
+const brokenFoot2 = "Footguy";
+
+const smashedfoot2 = brokenFoot2.length;
+
+console.log(brokenFoot2, smashedfoot2);
