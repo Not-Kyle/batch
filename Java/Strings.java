ce3f0c118096b238303a807475821412bbe102f1
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-16 12:31:31 -0400

    Update Strings.java

---
+++ Java/Strings.java
@@ -0,0 +1,14 @@
+String newcclosure = "hi im ":
+int age = 13;
+double agetwo = 4.5;
+double real;
+age + agetwo = real;
+boolean not = true;
+
+public class writeage {
+  public static void main(String[] args) {
+    if (not) {
+     System.out.println(newclosure + real);
+    }
+  }
+}
