81aa83da06df4c2d48f2b13257110a111154c7f2
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-16 12:34:36 -0400

    Update Strings.java

--- Java/Strings.java
+++ Java/Strings.java
@@ -1,13 +1,20 @@
 public class writeage {
+
   public static void main(String[] args) {
+    
     String newcclosure = "hi im ":
     int age = 13;
     double agetwo = 4.5;
     double real;
     age + agetwo = real;
     boolean not = true;
-     if (not) {
+     
+    if (not) {
+      
       System.out.println(newclosure + real);
+    
     }
+  
   }
+
 }
