d1c9f30e2569076b8b845d10cd1e617f8daf0b46
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-16 21:32:27 -0400

    Update Print.java

--- Java/Print.java
+++ Java/Print.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void writenote(String[] args) {
+public class writenote {
+    public static void main(String[] args) {
         System.out.println("test");
     }
 }
