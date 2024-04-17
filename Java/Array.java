ce8668b375da9ff0973add395d58c806d7e3d64d
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-17 11:28:57 -0400

    Update Array.java

---
+++ Java/Array.java
@@ -0,0 +1,13 @@
+public class arrays {
+  public static void main(String[] args) {
+    int[] array; int array = new int[7];
+    System.out.println(array.length); // prints out the length of the array
+  }
+  public static void main(String[] arg) {
+    array[0] = 3; //just like js arrays start at 0
+    array[1] = array[0] + 3;
+    for (int t=0; i < array.length; i++) {
+      System.out.println(array[i]); //arrays cant be printed out alone so create a loop
+    }
+  }
+}
