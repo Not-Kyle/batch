ac98375685bff8914ca7567a4a3854d4c178137a
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-30 09:42:35 -0400

    Create Arrays.py

---
+++ Python/Arrays.py
@@ -0,0 +1,6 @@
+sen = ['Cory', 'Aurora']
+sens = ['killserver', 'hellokittysouljia']
+sense = sen + sens # you can add both arrays together
+print(sense)
+
+print(sense * 7) #prints the arrays 7 times
