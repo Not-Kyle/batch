d553720713b80b173d865aa5d36a0bcb6bbec276
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-13 11:54:59 -0400

    Update If&Else.java

---
+++ Java/If&Else.java
@@ -0,0 +1,13 @@
+public class Test {
+ private static void testing(String[] args) {
+  byte y = 7
+  byte z = 6
+  
+  if (y == z) {
+   System.out.println("True")
+  } else {
+   System.out.println("False")
+  }
+ }
+}
+  
