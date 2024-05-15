d3c9ddc43d03e842f08d6f3fd616b0b0146409e7
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 04:15:20 -0400

    Create For Loop.java

---
+++ Java/For Loop.java
@@ -0,0 +1,18 @@
+class A {
+    private String random = "Hi";
+}
+public class test {
+    public static void main(String[] args) {
+        byte = 1;
+        char = 'g';
+        for (;i < 2;) {
+            System.out.println(char + byte);
+        }
+    }
+    public static void other(String[] args) {
+        for (i = 0; i < 3; i++) {
+            A a=new A();
+            System.out.println(a.random);
+        }
+    }
+}
