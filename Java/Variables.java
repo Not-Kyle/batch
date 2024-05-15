1740a6f151b26e69dd4e6805d255362c72ae69f5
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 08:53:17 -0400

    Create Variables.java

---
+++ Java/Variables.java
@@ -0,0 +1,14 @@
+public class Yes {
+ public static void main(String[] args) {
+  byte b1 = 1; //bytes
+  short b2 = 2; //bytes
+  int b4 = 4; //bytes
+  long b8 = 8; //bytes
+  float f4 = 3.99f; // four bytes 
+  double d8 = 7.99; // eight bytes
+  boolean bit1 = true; // one bit
+  char c2 = 'c'; // two bytes
+  String s1 = b1 + "" + b2 + "" + b4 + "" + b8 + "" + f4 + "" + d8 + "" + bit1 + "" + c2);
+  System.out.println(s1);
+ }
+}
