cc132de10eb026881004e33b7de026159c20b58d
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 04:46:38 -0400

    Create While Loop.java

---
+++ Java/While Loop.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        double m1 = 21492340;
+        int m2 = 4930;
+        char m3 = "d";
+        boolean m4 = true;
+        do {
+            System.out.println(m1 + m2 + m3);
+        } while (m4)
+    }
+    public static void other(String[] args) {
+        while (true) {
+            System.out.println(m1 + m2 + m3);
+        }
+    }
+    public static void others(String[] args) {
+        byte count = 1;
+        while (count <= 12) {
+            System.out.println(count); i++; // counts up to 12
+        }
+    }
+}
