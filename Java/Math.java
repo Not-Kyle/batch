4d1c1c52e402c085ad7675f52c4c23e242962b8b
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 13:43:23 -0400

    Create Math.java

---
+++ Java/Math.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.max(0.1,5); // Prints the max number
+  }
+  public static void other(String[] args) {
+    System.out.println(Math.min(0.1,5); // Prints the minimum number
+  }
+}
+// ** PERSONAL COMMENTS **
+// Q: Why is this useful
+// A: Lets say you have a command for a speed, this command using the Math.max will tell you the highest speed you can go
+public class Aura {
+  public static void others(String[] args) {
+    int i1 = 16;
+    System.out.println(Math.sqrt(i1)); // Prints the square root of i1
+  }
+  public static void more(String[] args) {
+    float f1 = -7.3f;
+    System.out.println(Math.abs(f1)); //Prints the absolute value of f1
+  }
+  public static void extra(String[] args) {
+    int i2 = (int) (Math.random() + 100);
+    System.out.println(i2); // Random number gen
+  }
+}
