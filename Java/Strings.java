c83c94d6eda7341205ad95d2487ff0ef64d56cd9
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 12:54:03 -0400

    Update Strings.java

--- Java/Strings.java
+++ Java/Strings.java
@@ -1,23 +1,42 @@
 public class writeage {
-
   public static void main(String[] args) {
-    
     String newcclosure = "hi im ":
     int age = 13;
     double agetwo = 4.5;
     double real;
     age + agetwo = real;
-    boolean not = true;
-     
-    if (not) {
-      
+    boolean not = true; 
+    if (not) { 
       System.out.println(newcclosure + real);
-    
     }
-  
   }
- public static void other(String[] args) {
-  final int num = 4; // use the keyword Final to make a variable unchangable and read only
-  System.out.println(num);
- }
+  public static void other(String[] args) {
+    final int num = 4; // use the keyword Final to make a variable unchangable and read only
+      System.out.println(num);
+  }
+}
+
+//Add-ons (May 15, 2024)
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Find hookfunction in my script";
+        System.out.println(s1.indexOf('hookfunction')); //find the keyword hookfunction
+    }
+    public static void other(String[] args) {
+        String s2 = "Find setmetatable in my script";
+        boolean b1 = true;
+        if b1 {
+            System.out.println(s2.toUpperCase()); // makes it completely uppercase
+            System.out.println(s2.toLowerCase()); // makes it completely lowercase
+        }
+    }
+    public static void others(String[] args) {
+        String s3 = "Find newcclosure in my script";
+        boolean b2 = true;
+        if b2 {
+            System.out.println("My string is " + s3.length() + " characters long"); // tells me the length of the string
+        } else {
+            System.out.println("null"); //just for fun, idk how to do (return nil) in java yet, but its null and not nil
+        }
+    }
 }
