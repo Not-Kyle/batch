284f6a1b0c8c1e964222ff4ff978601762ec4ad9
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-15 07:03:28 -0400

    Update Array.java

--- Java/Array.java
+++ Java/Array.java
@@ -4,7 +4,7 @@ public class arrays {
     System.out.println(array.length); // prints out the length of the array
   }
   public static void main(String[] arg) {
-    array[0] = 3; //just like js arrays start at 0
+    array[0] = 3; //just like js, arrays start at 0
     array[1] = array[0] + 3;
     for (int t=0; i < array.length; i++) {
       System.out.println(array[i]); //arrays cant be printed out alone so create a loop
