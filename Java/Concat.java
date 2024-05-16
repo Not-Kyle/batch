e949c5eb711bb2de070cac7aaef165d187389a12
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-16 07:12:31 -0400

    Update Concat.java

--- Java/Concat.java
+++ Java/Concat.java
@@ -1,7 +1,16 @@
 public class name {
-  public static void main(String[] args) {
-    int num = 13;
-    String sen = "My number is ";
-    System.out.println(sen + num);
-  }
+   public static void main(String[] args) {
+      int num = 13;
+      String sen = "My number is ";
+      System.out.println(sen + num);
+   }
+   public static void other(String[] args) {
+      int ID = 4920;
+      short SD = 17;
+      if (ID != SD) {
+        System.out.println(ID.concat(SD));
+      } else {
+        System.out.println("null");
+      }
+   }
 }
