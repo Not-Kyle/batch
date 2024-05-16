0efb3122176d0dcfd13f7064b14fa9d79949495b
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-16 03:47:07 -0400

    Create Booleans.java

---
+++ Java/Booleans.java
@@ -0,0 +1,20 @@
+public class Test {
+    public static void main(String[] args) {
+        int Velocity = 5;
+        int Range = 2;
+        if (Velocity >= Range) {
+            System.out.println("Velocity is greater");
+        } else {
+            System.out.println("Range is greater");
+        }
+    }
+    public static void other(String[] args) {
+        int Viscosity = 5;
+        short Thickness = 1;
+        if (Viscosity > Thickness) {
+            System.out.println("Viscosity is greater");
+        } else {
+            System.out.println("Thickness is greater");
+        }
+    }
+}
