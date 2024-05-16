9975571595c5349518833bda043d640b373cb889
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-16 09:05:24 -0400

    Update Switch.java

--- Java/Switch.java
+++ Java/Switch.java
@@ -1,1 +1,15 @@
-g
+public class B {
+   public static void main(String[] args) {
+      int time = 24;
+      switch (time) {
+         case 12:
+            System.out.println(“Day”);
+         break;
+         case 24:
+            System.out.println(“Night”);
+         break;
+         default:
+            System.out.println(“null”);
+      }
+   }
+}
