3739c430fbaa08371d0d86157fd3258788082918
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-16 18:30:18 -0400

    Update For Loop.java

--- Java/For Loop.java
+++ Java/For Loop.java
@@ -3,10 +3,10 @@ class A {
 }
 public class test {
     public static void main(String[] args) {
-        byte = 1;
-        char = 'g';
+        byte r = 1;
+        char g = 'g';
         for (;i < 2;) {
-            System.out.println(char + byte); // going to print g1 two times
+            System.out.println(g + r); //going to print g1 two times
         }
     }
     public static void other(String[] args) {
@@ -15,4 +15,16 @@ public class test {
             System.out.println(a.random); //going to print nothing since it can not define the private keyword in line two
         }
     }
+}
+
+// Add-ons (May 16, 2024)
+public class b {
+   public static void extra() {
+      for (int x = 3; x < 7; x++) {
+         System.out.println(x)
+         for (short y = 4; y < 7; y++) {
+            System.out.println(y);
+         }
+      }
+   }
 }
