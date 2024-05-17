ecb029c53d893c522da8af1c603f9b934869af10
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-17 15:36:12 -0400

    Update Break&Continue.java

--- Java/Break&Continue.java
+++ Java/Break&Continue.java
@@ -1,1 +1,10 @@
-g
+public class B {
+   public static main(String[] args) {
+      for (short Maps = 0; Maps < 10; Maps++) {
+         if (Maps = 10) {
+            break; //Stops the loop when the variable is equal to 10
+         }
+       System.out.println(Maps);
+      }
+   }
+}
