411e0636b74fb66466f472da128d75808042b3b5
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-20 11:56:31 -0400

    Update Functions.java

--- Java/Functions.java
+++ Java/Functions.java
@@ -8,3 +8,7 @@ public class Main {
       hook(); // Calling the function
    }
 }
+/*
+Static: This keyword means the function belongs to the Main class and is not an object of the Main class
+Void: This keyword means the function does not have a return value
+*/
