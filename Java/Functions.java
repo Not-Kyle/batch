85672d7ffdbc1b2cc5d52d429c774106d39e2f98
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-20 12:47:07 -0400

    Update Functions.java

--- Java/Functions.java
+++ Java/Functions.java
@@ -12,3 +12,20 @@ public class Main {
 Static: This keyword means the function belongs to the Main class and is not an object of the Main class
 Void: This keyword means the function does not have a return value
 */
+public class Water {
+    static void hookfunction(String newcclosure) {
+        System.out.println("syn" + newcclosure);
+    }
+    static void Command(String x, int y) {
+        System.out.println(x + y);
+    }
+    public static void main(String[] args) {
+        String Dot = "."
+        String s1 = Dot + "request";
+        String s2 = Dot + "protect_gui";
+        hookfunction(s1); // Prints syn.request
+        hookfunction(s2); // Prints syn.protect_gui
+        //
+        Command("Cory, ", 18); // Prints Cory 18
+    }
+}
