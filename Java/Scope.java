83089220f7c7d9dbbc6336f3bb0b5ed932bfa6c7
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-23 05:25:46 -0400

    Create Scope.java

---
+++ Java/Scope.java
@@ -0,0 +1,9 @@
+public class Func {
+	public static void main(String[] args) {
+		{ // Begin
+			double Haunted = 9e9;
+			System.out.println(Haunted);
+		} // End
+		int Mound = (int) Haunted; // Can not call Haunted
+	}
+}
