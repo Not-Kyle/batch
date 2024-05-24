9c5facdb1605d91e6787277aed8d694bb7eee478
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-24 06:16:13 -0400

    Create Recursion.java

---
+++ Java/Recursion.java
@@ -0,0 +1,15 @@
+public class Func {
+  public static void main(String[] args) {
+    int wiker = sematary(7);
+    System.out.println(wiker); // Outputs 28;
+  }
+  public static int sematary(int start) {
+    if (start > 0) {
+      return start + sematary(start - 1);
+    } else {
+      return 0;
+    }
+  }
+}
+// Recursion will basically add every number between 7, so
+// 0+1+2+3+4+5+6+7 = 28
