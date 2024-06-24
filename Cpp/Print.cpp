0fbd376538d1dc7ea18c379284e6e8d1c55ec45e
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-23 19:17:01 -0700

    Update Print.cpp

---
+++ Cpp/Print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+   std::cout << "Hi";
+   return 0
+}
+
+//OUTPUT
+//Hi
+
+//iostream: this is a file including all basic CPP functions
+//std: meaning standard
+//cont: meaning char out
+//return 0 means theres no errors
