7d3e8a15ab26df54d358f6710e7094c0403712d8
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-24 23:36:03 -0700

    Create Variables.cpp

---
+++ C++/Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    int Age = 18;
+    std::cout << Age;
+
+    double Fast = 9.4; // A Float
+    std::cout << Fast;
+
+    char X = 'X';
+    std::cout << X;
+
+    std::string name = "Cory"; 
+    std::cout << name;
+
+    bool Reloading = false;
+    std::cout << Reloading;
+
+    Age = 17;
+    std::cout << Age;
+}
