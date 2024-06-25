ec1a05edd7f82a64efceae2eff7627c140d0b084
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-24 18:38:26 -0700

    Create Nextline.cpp

---
+++ C++/Nextline.cpp
@@ -0,0 +1,22 @@
+// -- > USING \n < -- \\
+
+#include <iostream>
+
+int main(){
+  std::cout << "Yes\n";
+  std::cout << "No";
+  return 0;
+}
+
+// -- > USING endl < -- \\
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Yes" << endl;
+  cout << "No";
+  return 0;
+}
+
+// -- > endl must be used with 'using namespace std' < -- \\
