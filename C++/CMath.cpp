6030b25acbeced2ecb71d03cdd49b454e0fdb560
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-26 10:22:12 -0700

    Update CMath.cpp

--- C++/CMath.cpp
+++ C++/CMath.cpp
@@ -1,1 +1,30 @@
+// Must use #include <cmath>
+
+// -- > Max  and Min < -- \\
+
+  #include <iostream>
+  #include <cmath>
+  using namespace std;
+  
+  int main() {
+  	int x = 7, y = 10;
+  	cout << max(x, y) << endl;
+  	cout << min(x, y);
+  	return 0;
+  }
+
+// -- > Log, Sqrt, Round < -- \\
+
+  #include <iostream>
+  #include <cmath>
+  using namespace std;
+  
+  int main() {
+  	int x = 7, y = 10;
+  	float z = 4.3;
+  	cout << log(x) << std::endl; // logarithm
+  	cout << sqrt(y) << std::endl;
+  	cout << round(z) << std::endl;
+  	return 0;
+  }
 
