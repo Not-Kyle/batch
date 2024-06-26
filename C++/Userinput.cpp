c324baadb02d58a1d1ed890cdd793a889649d039
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-26 09:32:39 -0700

    Update Userinput.cpp

--- C++/Userinput.cpp
+++ C++/Userinput.cpp
@@ -7,3 +7,18 @@ int main() {
     cin >> X;
     cout << "debugging: " << X;
 }
+
+// OR -- > getline() < --
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string AddCommand;
+	cout << "Command: ";
+	getline(cin, AddCommand); 
+	cout << "Executing: " << AddCommand;
+}
+
+// getline() prints the entire string rather than how cin just prints the first word of the string
