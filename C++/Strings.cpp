cee359fcb2f5c9c19446ab6e0daa3f20bc8083da
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-25 18:38:38 -0700

    Update Strings.cpp

--- C++/Strings.cpp
+++ C++/Strings.cpp
@@ -61,3 +61,53 @@
         cout << "CMD: " << X << endl << X[0]; // Prints out the first letter of the string
         return 0;
     }
+
+// OR
+
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    
+    int main() {
+        string X = "THREE";
+        cout << "CMD: " << X[X.size() - 5]; // or length
+        return 0;
+    }
+
+// -- > Change Characters or Print Characters < -- \\
+
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    
+    int main() {
+        string X = "THREE";
+        X[1] = 'E'; // Changes H
+        cout << "CMD: " << X; // Outputs TEREE
+        return 0;
+    }
+
+// OR 
+
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    
+    int main() {
+        string X = "THREE";
+        X.at(1) = 'L'; // Changes H to L
+        cout << "CMD: " << X;
+        return 0;
+    }
+
+ // Prints Single Characters
+
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    
+    int main() {
+        string X = "THREE";
+        cout << "CMD: " << X.at(1); // Outputs H
+        return 0;
+    }
