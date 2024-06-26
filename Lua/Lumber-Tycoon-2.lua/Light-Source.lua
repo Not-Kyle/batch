d3ce3194df9932ee99b96fc48bdb6a18bef16cbc
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-25 18:24:07 -0700

    Update Light-Source.lua

--- Lua/Lumber-Tycoon-2.lua/Light-Source.lua
+++ Lua/Lumber-Tycoon-2.lua/Light-Source.lua
@@ -1,4 +1,3 @@
 getgenv().Host = game:GetService'Players'.LocalPlayer
-
 getgenv().Light = Instance.new('PointLight', Host.Character:FindFirstChild'HumanoidRootPart')
 Light.Range = 50 --change this number
