fe49b81f7f64f62a4e85d6bdc53f12e688679b24
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-06-28 19:37:56 -0700

    Create Variables.cpp

---
+++ C++/Notes.cpp/Variables.cpp
@@ -0,0 +1,5 @@
+/*
+  Initialized = The object is given a known value at the point of definition.
+  Assignment = The object is given a known value beyond the point of definition.
+  Uninitialized = The object has not been given a known value yet.
+*/
