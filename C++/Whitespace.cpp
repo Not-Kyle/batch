332d0444c2ad872cd4d44012eff31b096fd916bf
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-11 21:23:56 -0700

    Create Whitespace.cpp

---
+++ C++/Whitespace.cpp
@@ -0,0 +1,50 @@
+// Code or variables can be defined on the next line
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout
+		<< "YES";
+
+	const int X
+		= { 5 };
+
+	cout
+		<< X;
+
+	return 0;
+}
+
+// Or, Code can be spacedout
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "YES";
+
+	const int X = { 5 };
+
+	cout << X;
+
+	return 0;
+}
+
+// Code can also be spacedout as much as you personally want
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout		<< "YES";
+
+	const int X = { 5 };
+
+	cout		<< X;
+
+	return		0;
+}
