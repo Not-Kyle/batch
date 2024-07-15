9244105d3cb5f65fdc2a3189c24f437fcba9610d
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-15 01:31:21 -0400

    Create Functions.cpp

---
+++ C++/Functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int one{ 1 }, two{ 2 }, three{ 3 }, four{ 4 }, five{ three + two };
+
+int returnFive() {
+	return five;
+}
+
+int main() {
+
+	int uniformFive{ returnFive() };
+
+	std::cout << uniformFive;
+
+	return 0;
+}
