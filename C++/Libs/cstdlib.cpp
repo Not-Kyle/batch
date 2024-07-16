f47b1941310456526198b7aa07aab501b5d9d50c
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-16 01:49:06 -0700

    Create cstdlib.cpp

---
+++ C++/Libs/cstdlib.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	std::cout << "Works?";
+
+	return EXIT_SUCCESS; // The same as Return 0
+}
+
+// OR
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	std::cout << "Works?";
+
+	return EXIT_FAILURE; // The same as Return 1
+}
