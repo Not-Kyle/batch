9c3bc5509240b76a8ef0e086b6d50762e1c53c9f
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-16 01:14:11 -0700

    Update Functions.cpp

--- C++/Functions.cpp
+++ C++/Functions.cpp
@@ -3,12 +3,12 @@
 int one{ 1 }, two{ 2 }, three{ 3 }, four{ 4 }, five{ three + two };
 
 int returnFive() {
-	return five;
+    return five;
 }
 
 int main() {
     int uniformFive{ returnFive() };
 
-	std::cout << uniformFive;
-	return 0;
+    std::cout << uniformFive;
+    return 0;
 }
