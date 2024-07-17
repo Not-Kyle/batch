db15e3347564db2117d5cf7fdd50316e4df73b4e
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-16 18:01:36 -0700

    Update AddCommand.cpp

--- C++/Development/AddCommand.cpp
+++ C++/Development/AddCommand.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <fstream>
 
-[[maybe_unused]] char colonSpaced[]{": "}, letterF{'f'};
-std::string hostCommand{ "fuhrer" }, commandAbbreviated{"uhrer"};
-bool isOn{};
+[[maybe_unused]] char colonSpaced[]{ ": " }, letterF{ 'f' };
+std::string hostCommand{ "fuhrer" }, commandAbbreviated{ "uhrer" };
+bool isOn{}, isTrue{ true };
 
 std::string getUserInput(std::string route) {
     std::string addedCommand{};
-    
-    std::cout           << route;
+
+    std::cout << route;
     getline(std::cin, addedCommand);
 
     return addedCommand;
+}
+
+std::string writeFile(std::string fileName, std::string fileText) {
+    std::ofstream makeFile{ fileName };
+
+    makeFile << fileText;
+    makeFile.close();
+
+    return EXIT_SUCCESS;
 }
 
 int main() {
@@ -21,11 +31,21 @@ int main() {
     if (getCommand == hostCommand) {
         isOn = true;
         if (isOn) {
-            std::cout   << (char)toupper(letterF) << commandAbbreviated << " is now true";
+            std::cout << (char)toupper(letterF) << commandAbbreviated << " is now true";
+            writeFile("reinhard.java",
+                "public class reinhard {\n\t"
+                    "public static void main(String[] args) {"
+                        "\n\t\tSystem.out.println(\"Writing File\");"
+                    "\n\t}"
+                "\n}"
+            );
         }
-    } else if (getCommand != hostCommand) {
-        std::cout       << "Not a local command";
-    } else {
+    }
+    else if (getCommand != hostCommand) {
+        std::cout << "Not a local command";
+        return EXIT_FAILURE;
+    }
+    else {
         return EXIT_FAILURE;
     }
 
