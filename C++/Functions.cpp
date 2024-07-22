34795c8657d08d2b2bb7d4312bd7abc8cde794e0
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-21 23:45:09 -0700

    Update Functions.cpp

--- C++/Functions.cpp
+++ C++/Functions.cpp
@@ -12,3 +12,32 @@ int main() {
     std::cout << uniformFive;
     return 0;
 }
+
+// Arguments
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+//         Arg 1 v           Arg 2 v      Arg 3 v
+int Math(std::string method, int first, int second) { 
+	if (method == "Addition") {
+		return		first + second;
+
+	} else if (method == "Subtraction") {
+		return		first - second;
+
+	} else if (method == "Multiplication") {
+		return		first * second;
+
+	} else if (method == "Division") {
+		return		first / second;
+
+	} else {
+		return		EXIT_FAILURE;
+	}
+}
+
+int main() {
+	std::cout		<< Math("Multiplication", sqrt(6), 4);
+	return			EXIT_SUCCESS;
+}
