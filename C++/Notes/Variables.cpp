59ecca8d46fead82c526039a9ba531951a7cecde
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-22 05:49:46 -0700

    Update Variables.cpp

--- C++/Notes/Variables.cpp
+++ C++/Notes/Variables.cpp
@@ -2,9 +2,12 @@
   Initialized = The object is given a known value at the point of definition.
   Assignment = The object is given a known value beyond the point of definition.
   Uninitialized = The object has not been given a known value yet.
-
-  int A = {1}; Initalized
-  int B; Assignment
-  B = {2}
-  int C; Uninitialized
 */
+#include <iostream>
+
+int main() {
+  int A = {1}; //Initalized
+  int B; //Assignment
+  B = {2}
+  int C; //Uninitialized
+}
