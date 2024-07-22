f301a380162e8ac9a45ea1abf3bdbdb033f549d4
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-22 01:23:03 -0700

    Update Local Variable.cpp

--- C++/Notes/Local Variable.cpp
+++ C++/Notes/Local Variable.cpp
@@ -5,7 +5,7 @@ int main() {
 	float grade = 8.5; // Local Variable
 
 	std::cout << grade << std::endl;
-	return		EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 // A Local Variable is a variable locked in a function or code block, this variable is not accessable out of the curly bracets
