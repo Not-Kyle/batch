1288945654e8928415b20de0d36eb4eea7a3f964
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-09-24 20:05:53 -0700

    Update MileCalc.cpp

--- C++/Development/MileCalc.cpp
+++ C++/Development/MileCalc.cpp
@@ -17,21 +17,40 @@ float getUserInput(std::string route) {
 
 int main() {
     char answer{};
-    char y{ 'y' }, n{ 'n' };
-    float Time{};
+    const char y{ 'y' }, n{ 'n' };
+    float Time{}, moreTime{};
     [[maybe_unused]] std::string Hours{ " Hour(s)" }, Minutes{ " Minute(s)" };
 
     float getMiles{ getUserInput("Enter Miles: ") };
-    const float Miles{ getMiles };
+    float Miles{ getMiles };
         
     float getSpeed{ getUserInput("Enter Speed: ") };
-    const float Speed{ getSpeed };
+    float Speed{ getSpeed };
 
     std::cout << "Is there multiple roads? [y,n]: ";
     std::cin >> answer;
 
     if (answer == y) {
-        return EXIT_FAILURE; // Will work on this later
+        float getExtraMiles{ getUserInput("Enter Miles: ") };
+        float extraMiles{ getExtraMiles + Miles };
+
+        float getExtraSpeed{ getUserInput("Enter Speed: ") };
+        float extraSpeed{ getExtraSpeed + Speed };
+
+        Time = Miles * 1 / Speed;
+        float getTime{ Time * 60 };
+
+        moreTime = extraMiles * 1 / extraSpeed;
+        float addTime{ getTime + moreTime * 60 };
+
+        if (addTime >= 61) {
+            float getHours{ addTime / 60 };
+            std::cout << "Takes " << getHours << Hours;
+        }
+        else if (addTime <= 60) {
+            std::cout << "Takes " << floor(addTime) << Minutes;
+        }
+
     } else if (answer == n) {
 
         Time = Miles * 1 / Speed;
