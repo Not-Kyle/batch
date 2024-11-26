ba798973b9a3f2d13a39b1c6167b618416c7c201
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-11-25 20:47:33 -0800

    Create Draw_Triangle.lua

---
+++ Lua/Tests.lua/Draw_Triangle.lua
@@ -0,0 +1,12 @@
+local tri = Drawing.new("Triangle")
+tri.Visible = true;
+tri.PointA = Vector2.new(10, 76)
+tri.PointB = Vector2.new(100, 100)
+tri.PointC = Vector2.new(100, 100)
+tri.Color = Color3.fromRGB(102, 0, 204)
+tri.Thickness = 3
+tri.Filled = false
+tri.Transparency = 1
+
+--Sentinal Doc
+
