8c4e1761a990de9d6f209978781508047206d8f6
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-01-01 19:55:32 -0800

    Create Test.ts

---
+++ Typescript/Test.ts
@@ -0,0 +1,14 @@
+// Was bored and decided to take a look at TS
+
+enum GetTypes {
+    Yes = "Yes",
+    Hey = "Hey",
+}
+
+let SetConst: number = 65;
+
+if (GetTypes.Yes == "Yes") {
+    console.log(GetTypes.Yes)
+} else {
+    console.log(SetConst)
+}
