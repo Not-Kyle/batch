0cbea245fce9dae1f70eb46caaf6ffb2b1c31380
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-23 01:30:04 -0800

    Create valueOf.java

---
+++ Java/2025/Integer/valueOf.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String example = "57";
+
+        int value = Integer.valueOf(example); // Basically tonumber()
+        System.out.println(value);
+    }
+}
