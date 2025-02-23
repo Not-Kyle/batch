06ddb8fadc27c46ac46a1a94eee9a8f19fcfaed4
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-23 01:39:13 -0800

    Update Scanner.java

--- Java/2025/Scanner.java
+++ Java/2025/Scanner.java
@@ -16,3 +16,22 @@ Port:
 443
 Port Entree 443
 */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter: ");
+        double value = Double.parseDouble(userInput.nextLine());
+
+        System.out.println("Port: " + value);
+    }
+}
+
+/*
+Enter:
+3.5
+Port: 3.5
+*/
