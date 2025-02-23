f09b36859c39c78eb25ed2e8b28e611afec76c17
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-23 09:48:24 -0800

    Create Increase Counter.java

---
+++ Java/2025/Fors/Increase Counter.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        for (int index = 0; index < 11; index++) {
+            System.out.println(index);
+        }
+        
+    }
+}
