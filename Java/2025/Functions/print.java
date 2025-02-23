2e8086899e946f93bbf2b4df1448512d2572ea13
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-23 10:00:40 -0800

    Create print.java

---
+++ Java/2025/Functions/print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        print("Bruh");
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
