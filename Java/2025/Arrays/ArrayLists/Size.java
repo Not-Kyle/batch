42b704f05ba055ccb1e6fa47096c0aaa072eea33
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-24 13:45:12 -0800

    Create Size.java

---
+++ Java/2025/Arrays/ArrayLists/Size.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> network = new ArrayList<>();
+        network.add("[ERROR]");
+        network.add("[ERROR]");
+        network.add("[ERROR]");
+
+        int sizeOf = network.size();
+        System.out.println(sizeOf);
+    }
+}
