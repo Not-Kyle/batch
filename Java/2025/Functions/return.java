caa9ce6a805f2f7f433901593d504bbb6d04756a
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-23 17:23:45 -0800

    Update return.java

--- Java/2025/Functions/return.java
+++ Java/2025/Functions/return.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
+        System.out.println("Symbol: ");
         String first = userInput.nextLine();
-        float second = userInput.nextFloat();
-        float third = userInput.nextFloat();
+
+        System.out.println("X Value: ");
+        float second = Float.valueOf(userInput.nextFloat());
+
+        System.out.println("Y Value: ");
+        float third = Float.valueOf(userInput.nextFloat());
 
         System.out.println(math(first, second, third));
     }
@@ -14,12 +19,14 @@ public class Main {
     public static float math(String symbol, float x, float y) {
         if (symbol.equals("+")) {
             return x + y;
-        } else if (symbol.equals("-")) { // Relearn switch statements
+        } else if (symbol.equals("-")) {
             return x - y;
         } else if (symbol.equals("*")) {
             return x * y;
         } else if (symbol.equals("/")) {
             return x / y;
+        } else {
+            System.out.println("[ERROR]: Invalid symbol");
         }
 
         return 0;
