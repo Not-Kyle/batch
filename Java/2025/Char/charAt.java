8221409444199a8bc1b4e91076dd908e8db5af57
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-24 20:07:57 -0800

    Create charAt.java

---
+++ Java/2025/Char/charAt.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Cory";
+        char c = text.charAt(0); // Indexes C from text
+
+        System.out.println(c);
+    }
+}
