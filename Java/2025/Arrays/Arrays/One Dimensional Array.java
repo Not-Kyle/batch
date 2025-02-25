14584e439959e03a43a2839a8722c852803dce8a
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-24 18:49:23 -0800

    Create One Dimensional Array.java

---
+++ Java/2025/Arrays/Arrays/One Dimensional Array.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float[] data = {5, 4, 7, 2, 3};
+
+        // listArray(data);
+        System.out.print("Data Value: " + data.length); // The number of elements
+    }
+}    
