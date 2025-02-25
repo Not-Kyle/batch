27d27451d2b1db2e3529ac5c3cce40022f86a4a2
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-24 19:56:48 -0800

    Create contains.java

---
+++ Java/2025/Strings/contains.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "MynameisCory";
+
+        if (text.contains("Cory")) {
+            System.out.println("Has Cory");
+        }
+    }
+}
