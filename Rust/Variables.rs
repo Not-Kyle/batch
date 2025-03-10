473bc0682f4ff3911b46ff252bb005ed705e6603
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-03-10 10:39:01 -0700

    Update Variables.rs

--- Rust/Variables.rs
+++ Rust/Variables.rs
@@ -2,3 +2,16 @@ fn main() {
     let age = 18;
     println!("Age {}", age)
 }
+
+////
+
+fn main() {
+    let mut age = 18; // Allows the variable to be changed
+    println!("Age {}", age);
+
+    let name = "Cory";
+    println!("Name {}", name);
+
+    age = 20;
+    println!("Age {}", age);
+}
