7c0de8126ca2c42ae7a37d3b9055e71fe5b8cc19
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-03-10 10:48:56 -0700

    Create Type Checking.rs

---
+++ Rust/Type Checking.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let letters: char = 'C';
+    let numbers: i32 = 2;
+    let floats: f32 = 4.5;
+
+    let positive: u32 = 5.4; // Cannot store negative numbers
+    println!("{}", letters)
+}
+
+/*
+i8 i16 i32 i64 i128
+f8 f16 f32 f64 f128
+u8 u16 u32 u64 u128
+*/// bits not bytes
