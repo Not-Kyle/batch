b82890a9a3395c87ef8e9f31bf1d463824e5c828
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-03-10 11:00:05 -0700

    Create Booleans.rs

---
+++ Rust/Booleans.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let aim_assist: bool = false;
+    let aimbool_toutf: i32 = aim_assist as i32;
+
+    println!("Aim Assist: {}", aim_assist);
+    println!("Aim Assist Binary: {}", aimbool_toutf);
+}
