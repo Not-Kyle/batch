29584f0822bbf2838138174bab8d8d3c7eb52212
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-05-03 09:38:40 -0700

    Create Main.java

---
+++ Java/2025/DEVELOPMENT/Playlist/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Playlist tracks = new Playlist();
+        tracks.addSong("Ghost Mountain, Stalks");
+        tracks.addSong("Elusin, Synfuels Bonus CD Track 11");
+        tracks.addSong("Ghost Mountain, Frozen Swimming Pool");
+        tracks.addSong("Sematary, King Of The Graveyard");
+        tracks.removeSong("Sematary, King Of The Graveyard");
+
+        tracks.printSongs();
+    }
+}
+// https://java-programming.mooc.fi/part-6/1-objects-within-objects
