405038e99a6f6ea34f04ca66638c5cc233a2adcc
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-05-03 09:38:23 -0700

    Create Playlist.java

---
+++ Java/2025/DEVELOPMENT/Playlist/Playlist.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private ArrayList<String> songs;
+
+    public Playlist() {
+        this.songs = new ArrayList<>();
+    }
+
+    public void addSong(String track) {
+        this.songs.add(track);
+    }
+
+    public void removeSong(String track) {
+        this.songs.remove(track);
+    }
+
+    public void printSongs() {
+        for (String index : this.songs) {
+            System.out.println(index);
+        }
+    }
+}
