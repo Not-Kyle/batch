11ff85105f1cb093548fd6767945373d969af86c
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-12-09 03:27:47 -0800

    Refactor sigma function for operator handling

--- Lua/math/Sigma Notation (math.sigma).lua
+++ Lua/math/Sigma Notation (math.sigma).lua
@@ -1,37 +1,23 @@
-local math = {}; 
+local math = {};
 math.__index = math
 
+local Operator = {
+    ['+'] = function(X: number, I: number) return X + I end,
+    ['-'] = function(X: number, I: number) return X - I end,
+    ['*'] = function(X: number, I: number) return X * I end,
+    ['/'] = function(X: number, I: number) return X / I end,
+}
+
 function math.sigma(Start: number, End: number, X: number, Symbol: string)
+    if not Operator[Symbol] then return end
+
     local Sum = 0;
-    local Operator;
 
-    local Symbols = {'+', '-', '*', '/'}
-
-    if table.find(Symbols, Symbol) then
-        Operator = tostring(Symbol)
-    end
-
-    for i = tonumber(Start), tonumber(End), 1 do
-        if Operator == '+' then
-            Sum += X + i
-        end
-
-        if Operator == '-' then
-            Sum += X - i
-        end
-
-        if Operator == '*' then
-            Sum += X * i
-        end
-
-        if Operator == '/' then
-            Sum += X / i
-        end
+    for Index = tonumber(Start), tonumber(End), 1 do
+        Sum += Operator[Symbol](X, Index)
     end
 
     return Sum
 end
 
 print(math.sigma(2, 7, 3, '*'))
-
--- Let me know if there is a better way to write this (This is just the basics of Sigma Notation)
