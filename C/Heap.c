42a01e54b17d4031dda4a7570d82bebd5ee76e6e
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-02-25 14:47:06 -0800

    Update Heap.c

--- C/Heap.c
+++ C/Heap.c
@@ -6,7 +6,7 @@
  *
  * Heap is primarily managed through two functions, "malloc" (memory allocation) and "free".
  *
- * Requesting Memory: When requesting memory you ask the OS for a specific number of bytes using malloc.
+ * Requesting Memory: When requesting memory you ask the C RUNTIME memory allocator for a specific number of bytes using malloc.
  * Malloc doesn't give you the variable. It gives you the pointer, the memory address to the start of that block.
  * The memory stays yours until you tell the OS you're done with it. It does not matter if the function that created it ends; the data remains.
  *
@@ -15,9 +15,13 @@
  *
  * Heap is managed by a memory allocator (Your software) that talks to the OS.
  * In low-level systems like UNIX the Heap starts right after your programs static data.
- * 
+ *
  * Heap in a near ASM view, to get more memory. The C lib makes a system call like "brk" or "sbrk" to the OS kernal. It will tell the kernal to move the program break line further up in memory to give more room.
  * The space between the old break and the new break is your raw Heap.
+ *
+ * Modern Systems use nmap instead of brk or sbrk as these are legacy.
+ *
+ * malloc -> allocator -> nmap
  */
 
 int main(void) {
