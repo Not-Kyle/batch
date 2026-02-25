3399987e3e8cba01df896b34db31bb37ee6bcc7c
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-02-25 00:13:52 -0800

    Update Stacks.c

--- C/Stacks.c
+++ C/Stacks.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+
 /*
  * What is a stack?
  * A stack is a region of RAM used to store data for functions
  *
- * When you call a stack, you're creating what's called a stack frame.
+ * When you call a function, you're creating what's called a stack frame.
  * Everytime you call a function, a new stack frame is pushed onto the stack.
  *
  * What happens inside the push?
@@ -13,11 +15,13 @@
  * A Stack Pointer is a special CPU register, what happens is when you push. The SP moves down. In most architectures the stack grows toward lower memory address to reserve space.
  *
  * When the CPU "Pops" that stack frame it means the CPU hit that functions return.
- * It wipes the SP back to where it was before the function started.
+ * It reverts the SP back to where it was before the function started.
  * The data is not deleted or zeroed out, the CPU just forgets its there.
  * After this the memory is considered free for the next function to overwrite.
  *
  * You can not return a pointer to a local variable in C
+ * Why is this?
+ * The C compiler thinks you know what you're doing, so it will let you return that pointer and your program will crash when you try to read that "garbage" memory.
  *
  * In simple terms.
  * The Stack: A region of memory managed by the CPU using a Stack Pointer.
@@ -26,3 +30,8 @@
  *
  * Popping does NOT erase data. It just moves the pointer. This is why uninitialized variables in C often contain garbage data from previous functions.
  */
+
+int main(void) {
+    printf("Test");
+    return 0;
+}
