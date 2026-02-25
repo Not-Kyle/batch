9d5a5b3556863582fc5c5f9408b26a098f452767
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-02-25 12:03:20 -0800

    Update Static Variables.c

--- C/Static Variables.c
+++ C/Static Variables.c
@@ -21,6 +21,37 @@
  * If you were to have two different .c files and both had the same variable name as; "static int words", it would not crash as they both have different memory addresses.
  */
 
+/*
+ * Why static variables are the enemy of Multithreading.
+ *
+ * In the modern program sense, threads are like having multiple CPU's running through your code at the exact same time.
+ * Since a static variable has a fixed memory address for the entire program, every thread that calls that function is fighting over the same piece of hardware.
+ *
+ * ASM Disaster
+ * To the CPU VARIABLE++ is actually three steps
+ * 1. Read: Go to the Data Segment address and load the value into the register.
+ * 2. Add: Add one to that register (variable)
+ * 3. Write: Store the new value back into the Data Segment address.
+ *
+ * Thread Collision.
+ * Thread A: Reads the value, example 10
+ * Thread B: Reads the same value 10 before Thread A can write it back.
+ * Thread A adds one and writes 11
+ * Thread B add one and writes 11
+ *
+ * This called Race Condition.
+ * Race Condition is when multiple threads perform the Read-Modify-Write cycle on the same static address at the same time, leading to data corruption.
+ *
+ * Why local variables are naturally Thread Safe.
+ * Every thread gets its own Stack.
+ *
+ * When Thread A calls a function, it pushes a stack frame onto its Stack.
+ * When Thread B calls the same function, it pushes a stack frame onto its own separate stack.
+ * Both threads never touch each others variables.
+ *
+ * In C++ you will encounter Signleton Pattern. These are almost always built using static variables. If you don't account for threads, your single object will get corrupted the moment two parts of your program try to use it at once.
+ */
+
 int main(void) {
     printf("Test");
     return 0;
