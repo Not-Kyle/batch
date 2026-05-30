27f244447cb93b74ffe946ea28950ea1365b63a3
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-05-30 03:16:28 -0700

    Update MawbornLoader.lua

--- Lua/MawbornLoader.lua
+++ Lua/MawbornLoader.lua
@@ -20,7 +20,7 @@ local Repository = {
     Domain = 'raw.githubusercontent.com',
     Owner = 'Not-Kyle',
     Name = 'mawborn.xml',
-    Commit = 'ffc9662', -- You can run any stable version of Mawborn!
+    Commit = 'f28d8f9', -- You can run any stable version of Mawborn!
 }
 
 local BaseUrl = Format('https://%s/%s/%s/%s/', Repository.Domain, Repository.Owner, Repository.Name, Repository.Commit)
